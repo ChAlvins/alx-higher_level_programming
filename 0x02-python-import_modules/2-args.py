9205ecf2028301ff390c3f8b82bbe2e8acbcab73
vpnchengo <charo.alvin@gmail.com>
2023-01-06 13:08:10 -0800

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    num_of_args = len(sys.argv) - 1
+    if num_of_args == 0:
+        print("0 arguments.")
+    elif num_of_args == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(num_of_args))
+    for i in range(num_of_args):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
