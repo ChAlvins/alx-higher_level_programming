5c72e9dff01e460a2f528f6f17ef16dee7a4a51a
vpnchengo <charo.alvin@gmail.com>
2023-01-23 11:17:03 +0000

    task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    list_elements = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end='')
+            list_elements += 1
+        print()
+    except IndexError:
+        pass
+    print()
+    return list_elements
