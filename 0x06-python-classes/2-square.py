5f6edc8e9cc6d9b86f16183b4245d4619548dcc6
vpnchengo <charo.alvin@gmail.com>
2023-01-24 19:26:49 +0000

    correction

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,11 +3,11 @@
 
 
 class Square:
-    """class named square
-    with a private attribute: size
-    """
+    """class with a private attribute: size"""
+
     def __init__(self, size=0):
         """iniatializing object"""
+
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
