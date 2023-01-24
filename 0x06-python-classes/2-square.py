3c588e0b08e4c11a9e2e0c7a70648af3755f9ded
vpnchengo <charo.alvin@gmail.com>
2023-01-24 19:20:38 +0000

    task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""defining class square"""
+
+
+class Square:
+    """class named square
+    with a private attribute: size
+    """
+    def __init__(self, size=0):
+        """iniatializing object"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size__ = size
