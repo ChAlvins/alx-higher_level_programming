29f14cf6957604d1ff3e95f92cf85c6c33311a38
vpnchengo <charo.alvin@gmail.com>
2023-01-24 12:29:15 +0000

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""a class Square that defines a square by:
+    Private instance attribute: size
+    instantiation with size (no type/value verification)
+    You are not allowed to import any module
+"""
+
+
+class Square:
+    """a class named square"""
+    def __init__(self, size):
+        """initializing the object"""
+        self.__size = size
