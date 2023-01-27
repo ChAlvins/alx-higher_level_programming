93b28d29aacfdc5dc76a5d5fb348bf291c440815
vpnchengo <charo.alvin@gmail.com>
2023-01-27 20:24:38 +0000

    correction

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -17,7 +17,10 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
+    '''
     if type(size) is float and size < 0:
         raise TypeError("size must be an integer")
+    '''
+
     for i in range(size):
         print("#" * size)
