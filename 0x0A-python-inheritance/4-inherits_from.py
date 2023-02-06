ebdb59f5ec6634f9bde1185977879b8a9684e797
vpnchengo <charo.alvin@gmail.com>
2023-02-06 14:51:55 +0000

    task4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""inheritance checker"""
+
+
+def inherits_from(obj, a_class):
+    """returns True if the object is an instance of a class
+    that inherited (directly or indirectly) from the specified class;
+    otherwise False.
+    """
+
+    if type(obj) != a_class:
+        return issubclass(type(obj), a_class)
+    else:
+        return False
