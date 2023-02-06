72df38fc1c41b355e36169e37c01280353513f6b
vpnchengo <charo.alvin@gmail.com>
2023-02-06 17:47:17 +0000

    task8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''module with a subclass inheriting from BaseGeometry'''
+
+
+class BaseGeometry:
+    '''class with a public instance raising an exception'''
+
+    def area(self):
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        '''instance validating value'''
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    '''Rectangle class inheriting from BaseGeometry'''
+
+    def __init__(self, width, height):
+        '''instatiation'''
+        self.integer_validator("height", height)
+        self.__height = height
+        self.integer_validator("width", width)
+        self.__width = width
