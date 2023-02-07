3a8e427e89e3859a7275d31a7e13577e0303c212
vpnchengo <charo.alvin@gmail.com>
2023-02-07 19:26:53 +0000

    correction

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,6 +8,6 @@ def read_file(filename=""):
     no need to manage file permission or file doesn't exist exceptions
     """
 
-    with open(filename, encoding="utf-8") as f:
+    with open(filename, mode="r", encoding="utf-8") as f:
         read_data = f.read()
         print(read_data, end='')
