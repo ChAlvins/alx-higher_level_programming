3a40a5899bb78933edd2355e083ae4798628f092
vpnchengo <charo.alvin@gmail.com>
2023-02-08 08:04:28 +0000

    task3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Module that contains a function that returns the JSON
+representation of an object
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """ Function that returns the JSON representation of an object
+    """
+
+    return json.dumps(my_obj)
