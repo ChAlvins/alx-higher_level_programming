9f6838d70fcb3b7ce5e1c445b86b8f92aa29918a
vpnchengo <charo.alvin@gmail.com>
2023-02-08 14:37:07 +0000

    correction

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,58 +1,36 @@
 #!/usr/bin/python3
-"""module with BaseGeometry class"""
+'''module with a subclass inheriting from BaseGeometry'''
 
 
 class BaseGeometry:
-    """Base Geometry class empty"""
+    '''class with a public instance raising an exception'''
 
     def area(self):
-        """Area output"""
-
         raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
-        """ that validates value"""
-
+        '''instance validating value'''
         if not isinstance(value, int):
-            raise TypeError(f'{name} must be an integer')
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(f'{name} must be greater than 0')
+            raise ValueError("{} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
-    """Empty rectangle class"""
+    '''Rectangle class inheriting from BaseGeometry'''
 
     def __init__(self, width, height):
-        """ constructor that takes two arguments:
-        width and height."""
+        '''instatiation'''
 
         self.__width = width
         self.__height = height
-        self.integer_validator('width', width)
-        self.integer_validator('height', height)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
 
     def area(self):
-        """area output"""
-
+        '''returns area of rectangle'''
         return self.__width * self.__height
 
     def __str__(self):
-        """method returns a string representation
-        of the rectangle
-        """
-
-        return f'[Rectangle] {self.__width}/{self.__height}'
-
-
-class Square(Rectangle):
-    """Empty square class"""
-
-    def __init__(self, size):
-        """a constructor that takes a single argument: size."""
-
-        super().__init__(size, size)
-
-    def area(self):
-        """area output"""
-
-        return self.__width * self.__height
+        '''string representation of the rectangle'''
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
