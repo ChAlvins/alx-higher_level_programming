bd51df8256a74f5345d70f7484cdf3a624a2cdcb
vpnchengo <charo.alvin@gmail.com>
2023-02-10 17:18:35 +0000

    task1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a base model class."""
+import json
+
+
+class Base:
+    """Represent the base model.
+    Represents the "base" for all other classes in project 0x0C.
+    Attributes:
+        __nb_objects (int): private class attribute
+    """
+
+    __nb_objects = 0
