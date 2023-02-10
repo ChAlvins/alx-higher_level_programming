1928deb084b5694679a204ef7ab903291d780b2c
vpnchengo <charo.alvin@gmail.com>
2023-02-10 04:16:09 -0800

    correction

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,17 +2,21 @@
 """module that adds all arguments to a Python list,
 and then save them to a file:"""
 import json
-import sys
-import os.path
+from sys import argv
 
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
-if os.path.isfile(filename):
-    obj = load_from_json_file(filename)
-else:
-    obj = []
-    obj.extend(sys.argv[1:])
-    save_to_json_file(obj, filename)
+
+try:
+    json_list = load_from_json_file(filename)
+
+except FileNotFoundError:
+    json_list = []
+
+for arg in argv[1:]:
+    json_list.append(arg)
+
+save_to_json_file(json_list, filename)
