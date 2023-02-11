4389fa6b00f7026f416425eaf1f0ee03156bc712
vpnchengo <charo.alvin@gmail.com>
2023-02-11 20:43:00 +0000

    task7

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -85,7 +85,11 @@ class Rectangle(Base):
     def display(self):
         """prints in stdout the Rectangle instance with the character #
         """
+        for i in range(self.y):
+            print()
         for i in range(self.height):
+            for j in range(self.x):
+                print(' ', end='')
             for j in range(self.width):
                 print('#', end=' ')
             print()
