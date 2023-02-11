e7169c5dedce2904330c2c3adf837be02da867ca
vpnchengo <charo.alvin@gmail.com>
2023-02-11 23:27:42 +0000

    task 12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -28,6 +28,46 @@ class Square(Rectangle):
         self.width = value
         self.height = value
 
+    def update(self, *args, **kwargs):
+        """assigning attributes
+        Args:
+            *args (ints): list of args-no keyworded args
+            1st argument represents id attribute
+            2nd argument represents size attribute
+            3rd argument represents x attribute
+            4th argument represents y attribute
+            **kwargs (dict): New key/value pairs of attributes.
+        """
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.size = arg
+                elif a == 2:
+                    self.x = arg
+                elif a == 3:
+                    self.y = arg
+                a += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "size":
+                    self.size = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
+
     def __str__(self):
         """return the print() and str() representation of the square"""
         return "[Square] ({}) {}/{} - {}".format(
