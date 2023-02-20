3040580e9d433dc5b9eb25755da42628196436e4
vpnchengo <charo.alvin@gmail.com>
2023-02-20 10:28:26 +0000

    task12-2

---
+++ 0x0B-python-input_output/12-pascal_triangle1.py
@@ -0,0 +1,8 @@
+def pascal_triangle(n):
+    triangle = []
+    for i in range(n):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+    return triangle
