2cae6ed5dbbfc7871817542085506f0715c95231
vpnchengo <charo.alvin@gmail.com>
2023-03-14 14:44:55 +0000

    listing all fields in a table

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,4 @@
+-- Lists all rows of the table first_table from the DB
+-- All fields should be printed
+-- database name will be passed as an argument of the mysql command
+SELECT * FROM first_table;
