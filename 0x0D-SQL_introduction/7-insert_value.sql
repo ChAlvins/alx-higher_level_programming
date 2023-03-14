ae4154d6021f33e3e1df35fd0490cc1015b289a5
vpnchengo <charo.alvin@gmail.com>
2023-03-14 14:53:34 +0000

    inserting new row in table

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,7 @@
+-- Inserts a new row into the table first_table
+-- New row:
+-- 	id = 89
+-- 	name = Best School
+-- The database name will be passed as an argument of the mysql command
+INSERT INTO first_table (id, name)
+VALUES (89, "Best School");
