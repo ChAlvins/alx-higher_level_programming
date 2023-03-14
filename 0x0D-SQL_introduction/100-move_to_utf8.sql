00034256785f5f3cc961e2b1bf1068e7d1869352
vpnchengo <charo.alvin@gmail.com>
2023-03-14 21:13:54 +0000

    sql alter

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,8 @@
+-- Converts the entire database hbtn_0c_0 to UTF8.
+-- convert all of the following to UTF8:
+--	Database hbtn_0c_0
+--	Table first_table
+--	Field name in first_table
+USE hbtn_0c_0
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
