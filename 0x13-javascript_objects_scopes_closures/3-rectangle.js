50e042bc4750647f3b05a6161ce26ca52bdafd41
vpnchengo <charo.alvin@gmail.com>
2023-04-12 18:21:09 +0000

    rectangle#3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// a Rectangle with constructor of two 2 args
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+module.exports = Rectangle;
