f5bb18757b84302419f085870ce28038b518be35
vpnchengo <charo.alvin@gmail.com>
2023-04-12 17:53:43 +0000

    first_constructor

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// a Rectangle with constructor of two 2 args
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
