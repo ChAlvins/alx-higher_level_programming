45a24a30d5e1c0624e4da34f98dfc59f80c6ea9e
vpnchengo <charo.alvin@gmail.com>
2023-04-12 00:27:24 +0000

    x_times_argv

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/*
+ * a script that prints x times “C is fun”
+ * Where x is the first argument of the script
+*/
+if (process.argv[2]) {
+  for (let i = 0; i < process.argv[2]; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
