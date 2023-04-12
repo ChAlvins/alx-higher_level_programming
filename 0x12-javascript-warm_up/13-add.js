f6dd80f9c79f91edcf1053212230e0eadfc21820
vpnchengo <charo.alvin@gmail.com>
2023-04-12 01:48:07 +0000

    add_file

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/*
+ * a function that returns the addition of 2 integers
+ * the function must be visible from outside
+ * The name of the function must be add
+*/
+exports.add = function (a, b) {
+  return (a + b);
+};
