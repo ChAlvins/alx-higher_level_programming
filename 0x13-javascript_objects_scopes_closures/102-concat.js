fd0826f8963130f6b393b26f9c06fe92dab4ad62
vpnchengo <charo.alvin@gmail.com>
2023-04-13 16:32:26 +0000

    concat files

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// a script that concats 2 files
+
+const argv = process.argv;
+const file = require('fs');
+const fileA = argv[2];
+const textA = file.readFileSync(fileA, 'utf8');
+const fileB = argv[3];
+const textB = file.readFileSync(fileB, 'utf8');
+const fileC = argv[4];
+file.writeFileSync(fileC, textA + textB);
