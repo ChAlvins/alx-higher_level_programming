0e0673b1231973f235bb3e46ff257b8c94fcdffe
vpnchengo <charo.alvin@gmail.com>
2023-04-15 13:49:20 +0000

    using format to filter by user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""script that takes in an argument and displays all values in the states
+   table of hbtn_0e_0_usa
+"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    username = args[1]
+    password = args[2]
+    name = args[3]
+    statename = args[4]
+    # Initializing the DB
+    db = MySQLdb.connect(host='localhost', user=username,
+                         passwd=password, db=name, port=3306)
+    # Getting the cursor
+    cur = db.cursor()
+    # Executing the msql statement and exit from db
+    num_rows = cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'
+                           ORDER BY id".format(state_name))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
