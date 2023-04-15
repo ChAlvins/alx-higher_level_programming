86a7e8daf8bbb95290a10276fbc8512756f816ab
vpnchengo <charo.alvin@gmail.com>
2023-04-15 13:09:29 +0000

    filter

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""a script that lists all states with a name starting with N (upper N)
+    from the database hbtn_0e_0_usa:
+"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    username = args[1]
+    password = args[2]
+    name = args[3]
+    # Initializing the DB
+    db = MySQLdb.connect(host='localhost', user=username,
+                         passwd=password, db=name, port=3306)
+    # Getting the cursor
+    curs = db.cursor()
+    # Executing the msql statement and exit from db
+    num_rows = curs.execute("""SELECT * FROM states WHERE name
+                    LIKE BINARY 'N%' ORDER BY states.id;""")
+    rows = curs.fetchall()
+    for row in rows:
+        print(row)
+    curs.close()
+    db.close()
