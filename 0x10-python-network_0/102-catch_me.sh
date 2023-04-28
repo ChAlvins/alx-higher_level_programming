f7dfad7b52a11006fde093a09e3088a0c2a77382
vpnchengo <charo.alvin@gmail.com>
2023-04-28 02:42:55 +0000

    updated

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # makes request to the URL (0.0.0.0:5000/catch_me) and display the body of the response (You got me!)
-curl -sLX PUT -d -H "Origin: HolbertonSchool" "user_id=98" 0.0.0.0:5000/catch_me
+curl -sLX PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
