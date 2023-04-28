e1a02ed00f571ce8633f9576551dac6e688a1c4f
vpnchengo <charo.alvin@gmail.com>
2023-04-28 19:41:06 +0000

    corrected fetching url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,7 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
+    url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
         the_page = response.read()
         content = the_page.decode('utf-8')
