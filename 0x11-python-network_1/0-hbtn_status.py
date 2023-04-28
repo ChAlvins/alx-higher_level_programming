a64e5a4a59073611a4a05e03bc30f2859caf590a
vpnchengo <charo.alvin@gmail.com>
2023-04-28 18:55:42 +0000

    fetching from url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""script that fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = 'https://intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        the_page = response.read()
+        content = the_page.decode('utf-8')
+        string = 'Body response:\n\t- type: {}\n\t- content: {}\n\t- \
+utf8 content: {}'.format(type(the_page), the_page, content)
+        print(string)
