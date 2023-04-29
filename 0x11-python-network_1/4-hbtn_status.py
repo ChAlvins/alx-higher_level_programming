65d22c46a0ef82783dc4160b2e8309eb7ddf086f
vpnchengo <charo.alvin@gmail.com>
2023-04-29 19:42:39 +0000

    updated

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,7 +6,7 @@ from requests import get
 
 
 if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
+    url = 'https://alx-intranet.hbtn.io/status'
     response = get(url)
     contents = response.text
     string = 'Body response:\n\t- type: {}\n\t- content: {}'.format(
