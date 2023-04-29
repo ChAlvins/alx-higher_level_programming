b60cf3604f28dae95f18ffc1d787386ffb7fe1dc
vpnchengo <charo.alvin@gmail.com>
2023-04-29 21:08:42 +0000

    updated

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -18,5 +18,5 @@ if __name__ == "__main__":
             print('{}: {}'.format(objects[i].get('sha'),
                                   objects[i].get('commit').get('author').
                                   get('name')))
-    except ValueError:
+    except Exception:
         pass
