12a33b79df476d18a269c44d13071c300e4cce66
vpnchengo <charo.alvin@gmail.com>
2023-04-29 20:05:57 +0000

    error code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+script that takes in a URL, sends a request to the URL
+and displays the body of the response.
+"""
+from requests import get
+import sys
+
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    response = get(url)
+    if response.status_code < 400:
+        print(response.text)
+    else:
+        print('Error code: {}'.format(response.status_code))
