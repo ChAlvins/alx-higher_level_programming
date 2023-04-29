9e0d0a302597f2bf70f51a011adf8e007897291b
vpnchengo <charo.alvin@gmail.com>
2023-04-29 20:35:22 +0000

    get my github credentials

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id
+"""
+from requests import get
+from requests.auth import HTTPBasicAuth
+from sys import argv
+
+if __name__ == "__main__":
+    url = 'https://api.github.com/user'
+    username = argv[1]
+    password = argv[2]
+    response = get(url, auth=HTTPBasicAuth(username, password))
+    obj = response.json()
+    print(obj.get('id'))
