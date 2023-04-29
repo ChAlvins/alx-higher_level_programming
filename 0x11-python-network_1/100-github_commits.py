4c289b87118b6a29bb34c27e7e2d0dc088c25c6b
vpnchengo <charo.alvin@gmail.com>
2023-04-29 21:02:01 +0000

    listing commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+lists 10 commits (from the most recent to oldest)
+of the repository “rails” by the user “rails”
+"""
+from requests import get
+from sys import argv
+
+
+if __name__ == "__main__":
+    username = argv[2]
+    repo = argv[1]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(username, repo)
+    response = get(url)
+    objects = response.json()
+    try:
+        for i in range(10):
+            print('{}: {}'.format(objects[i].get('sha'),
+                                  objects[i].get('commit').get('author').
+                                  get('name')))
+    except ValueError:
+        pass
