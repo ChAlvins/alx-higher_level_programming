60ca2341d712e1701fab59cf21d97a198151ddaa
vpnchengo <charo.alvin@gmail.com>
2023-08-21 19:42:45 +0300

    range object

---
+++ 0x01-python-if_else_loops_functions/my_range_loops.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""my range loop"""
+
+
+for item in range(5):
+    print(item)
+
+
+
+print("------------------------")
+
+number = range(0, 10, 2)
+for i in number:
+    print(i)
